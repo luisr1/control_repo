b2bb2bbc22669c2ac1e4f7ca782a906c357bccc2
luisr1 <amill@hotmail.com>
2022-05-10 16:29:49 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,15 +7,15 @@ class minecraft {
     source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
   }
   package {'jdk-18_linux-x64_bin':
-    provider = rpm,
-    source => 'https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.rpm',
-    ensure => installed,
+    provider => rpm,
+    source   => 'https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.rpm',
+    ensure   => installed,
   }
   package {'java':
     ensure => present,
   }
   file {'/opt/minecraft/eula.txt':
-    ensure => file,
+    ensure  => file,
     content => 'eula=true'
   }
   file {'/etc/systemd/system/minecraft.service':
