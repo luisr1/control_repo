d0075911a6b6a03e188c73145a73511fc81b94af
luisr1 <amill@hotmail.com>
2022-05-10 13:47:16 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4MS+6QDRyR6i7FGpPKLQnj6ZamEdruXCZGmsY9aUwgTvty5Ck5UsW1fStZ7foFqIg+GV36Gk6D/C5pNZ1U4JTa0nbTjlyI2lp+UigX4AEgTFbvPOPLydIjCvakFEaD6z4mAtqYHy2SuQEm75sX/O98omRWq4Jz+KQKDKDKQSOeXj7F0y+/ZBWrMQXVdTCjiQDTz5cPv+k+1ZFWx1/Jcy0Cn3B3WQNjs5kw6zcLBIKc/GtbpPopriKfxZABivqoUxdzB9Eefufm775jaP1O4QLkYPrfTHvl2ujvVDh6BByODZx/O/tB3CloQ5E1NPOzCRNAPnRqpQ9rfVwkqj6T97d',
   }
 }
