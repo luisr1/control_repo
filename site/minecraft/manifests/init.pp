2e43da4a7d070b6dbf9d80cea0c918674a146231
luisr1 <amill@hotmail.com>
2022-05-10 16:08:00 -0400

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,25 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/mminecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
+  }
+  package {'jdk-18_linux-x64_bin':
+    provider = rpm,
+    source => 'https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.rpm',
+    ensure => installed,
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true'
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
